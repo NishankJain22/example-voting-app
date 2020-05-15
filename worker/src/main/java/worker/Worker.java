988ba8d8a3edbf5541667d3cb7cdadb303274633
maheshkharwadkar <mahesh.s.kharwadkar@gmail.com>
2020-05-15 09:44:10 +0530

    Updating password

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -72,7 +72,7 @@ class Worker {
 
       while (conn == null) {
         try {
-          conn = DriverManager.getConnection(url, "postgres", "");
+          conn = DriverManager.getConnection(url, "postgres", "password");
         } catch (SQLException e) {
           System.err.println("Failed to connect to db - retrying");
           sleep(1000);
